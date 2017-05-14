0b73d79cd43023015c386efd3e58c1da5e471d83
Tobias <tobiastenberg@web.de>
2017-05-14 15:09:22 +0200

    Neues Ui Element - Ein button für die Menü Navigation

---
+++ CoffeeDrinkCount/CoffeeDrinkCount/UI_Elements/Button/ButtonNormalMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CoffeeDrinkCount.UI_Elements
+{
+    class ButtonNormalMenu : Button
+    {
+        //Konstruktor
+        public ButtonNormalMenu(string buttonText)
+        {
+            //Text des Button
+            Text = buttonText;
+
+            //Farben
+            TextColor = Color.White;
+            BackgroundColor = Color.FromHex("A36827");
+
+            //Ausrichtung
+            Margin = new Thickness(10, 4, 10, 4);
+            HorizontalOptions = LayoutOptions.FillAndExpand;
+            VerticalOptions = LayoutOptions.Start;
+            HeightRequest = 100;
+            
+            // WidthRequest = 300
+            // BorderWidth = 1,
+            //Image = new FileImageSource() { File = "timer.png" },
+            // Font = Font.SystemFontOfSize(NamedSize.Large),
+        }
+    }
+}
